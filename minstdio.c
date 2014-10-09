b3f7477ff4e74caa64a0b40389ae6b0a45f39073
drmetal <spaceorbot@gmail.com>
2014-10-10 08:54:06 +1300

    added more unit tests made corrections to bugs found while testing

--- minstdio.c
+++ minstdio.c
@@ -1,14 +1,21 @@
 /**
  * @defgroup minstdio minimal stdio
  *
- * tiny versions of some stdio functions. slower and less complete than the standard implementations, but can compile for a much smaller flash usage.
+ * tiny versions of some stdio functions.
+ * can compile for a much smaller flash usage than the standard implementations.
  *
  * To use:
  *
- *     the function _write must be defined somewhere in your project. this is typically defined in syscalls.c...
+ *     the functions _read, _write, _lseek, _ftell must be defined somewhere in your project.
+ *     this is typically defined in syscalls.c...
+ *
  *     just #include <stdio.h> like normal wherever you want to use it.
+ *
 \code
-// _write, function to write a characters to a device or file
+// examples of complete implementations may be found in syscalls.c in
+// the stm32_freertos project, https://github.com/drmetal/stm32_freertos
+
+// example of _write, function to write a characters to a device or file
 int _write(int file, char *buffer, unsigned int count)
 {
 	int n = EOF;
@@ -21,6 +28,31 @@ int _write(int file, char *buffer, unsigned int count)
 	}
 	return n;
 }
+// example of _read, function to read a characters from a device or file
+int _read(int file, char *buffer, unsigned int count)
+{
+	int n = EOF;
+
+	if(file == STDOUT_FILENO || file == STDERR_FILENO)
+	{
+		for(n = 0; n < (int)count; n++) {
+			phy_getc(*buffer++); // some code here to read a serial device, or memory, or whatever
+		}
+	}
+
+	return n;
+}
+// example of _lseek, function to change the position of the file pointer
+extern int _lseek(int file, int offset, int whence)
+{
+	return -1;
+}
+// example of _ftell, function to read the position of the file pointer
+extern long int _ftell(int fd)
+{
+	return -1;
+}
+
 \endcode
  *
  * supported string formatting:
@@ -52,6 +84,7 @@ int _write(int file, char *buffer, unsigned int count)
  *				-> ignore lh: 1234 1234 1234 1234 1234 1234
  * +		a + preceeds numeric formatted numbers when + is inserted. supported by i, u, d, f:
  *
+ *
  * this file is part of the minstdlibs project.
  *
  * https://github.com/drmetal/minstdlibs
@@ -64,8 +97,9 @@ int _write(int file, char *buffer, unsigned int count)
 
 #include <stdarg.h>
 #include <stdint.h>
-#include <unistd.h>
-#include <ctype.h>
+#include <unistd.h> // open(), close() etc
+#include <ctype.h> // isdigit() etc
+#include <fcntl.h>
 #include "minstdio.h"
 #include "minstdlib.h"
 #include "minstring.h"
@@ -79,43 +113,45 @@ int _write(int file, char *buffer, unsigned int count)
 #define LONG_FLAG 32
 #define SHORT_FLAG 64
 
-typedef char*(*putx_t)(char**, const char*);
+typedef char*(*putx_t)(int, char**, const char*);
 
 extern int _write(int file, char *buffer, unsigned int count);
+extern int _read(int file, char *buffer, unsigned int count);
+extern int _lseek(int file, int offset, int whence);
+extern long int _ftell(int fd);
 
-static inline char* phyputc(char** dst, const char*src)
+static inline char* phyputc(int fd, char** dst, const char*src)
 {
 	if(*src)
 	{
-		_write(STDOUT_FILENO, (char*)src, 1);
+		_write(fd, (char*)src, 1);
 		(*dst)++;
 	}
 	return *dst;
 }
 
-static inline char* phyputs(char** dst, const char*src)
+static inline char* phyputs(int fd, char** dst, const char*src)
 {
 	int l = strlen(src);
 	if(l > 0)
 	{
-		_write(STDOUT_FILENO, (char*)src, l);
+		_write(fd, (char*)src, l);
 		*dst += l;
 	}
 	return *dst;
 }
 
-static inline char* memputc(char** dst, const char*src)
+static inline char* memputc(int fd, char** dst, const char*src)
 {
-	if(*src)
-	{
-		**dst = *src;
-		(*dst)++;
-	}
+	(void)fd;
+	**dst = *src;
+	(*dst)++;
 	return *dst;
 }
 
-static inline char* memputs(char** dst, const char*src)
+static inline char* memputs(int fd, char** dst, const char*src)
 {
+	(void)fd;
 	while(*src)
 	{
 		**dst = *src;
@@ -125,22 +161,22 @@ static inline char* memputs(char** dst, const char*src)
 	return *dst;
 }
 
-static inline void plusflag(putx_t _put_char, unsigned int flags, char** dst)
+static inline void plusflag(int fd, putx_t _put_char, unsigned int flags, char** dst)
 {
 	if(flags&PLUS_FLAG)
 	{
 		char c = '+';
-		_put_char(dst, &c);
+		_put_char(fd, dst, &c);
 	}
 }
 
-static inline void hashflag(putx_t _put_str, unsigned int flags, char** dst)
+static inline void hashflag(int fd, putx_t _put_str, unsigned int flags, char** dst)
 {
 	if(flags&HASH_FLAG)
-		_put_str(dst, (const char*)"0x");
+		_put_str(fd, dst, (const char*)"0x");
 }
 
-static inline int strfmt(putx_t _put_char, putx_t _put_str, char** dst, const char * fmt, va_list argp)
+static inline int strfmt(int fd, putx_t _put_char, putx_t _put_str, char** dst, const char * fmt, va_list argp)
 {
 	double d;
 	int ret = -1;
@@ -242,21 +278,21 @@ static inline int strfmt(putx_t _put_char, putx_t _put_str, char** dst, const ch
 					{
 						case 'c':
 							c = (char)va_arg(argp, int);
-							_put_char(dst, &c);
+							_put_char(fd, dst, &c);
 						break;
 
 						case 's':
 							s = (char*)va_arg(argp, char*);
 							if(!s)
 								s = (const char*)"(null)";
-							_put_str(dst, s);
+							_put_str(fd, dst, s);
 						break;
 
 						case 'i':
 						case 'd':
 							i = (int)va_arg(argp, int);
 							if(i >= 0)
-								plusflag(_put_char, flags, dst);
+								plusflag(fd, _put_char, flags, dst);
 
 							itoa(i, intbuf, 10);
 							if((flags&ZERO_FLAG) || (flags&SPACE_FLAG))
@@ -271,15 +307,15 @@ static inline int strfmt(putx_t _put_char, putx_t _put_str, char** dst, const ch
 										padding--;
 										padbuf[padding] = padchar;
 									}
-									_put_str(dst, padbuf);
+									_put_str(fd, dst, padbuf);
 								}
 							}
-							_put_str(dst, intbuf);
+							_put_str(fd, dst, intbuf);
 						break;
 
 						case 'u':
 							u = (unsigned int)va_arg(argp, unsigned int);
-							plusflag(_put_char, flags, dst);
+							plusflag(fd, _put_char, flags, dst);
 
 							ditoa((int64_t)u, intbuf, 10);
 
@@ -295,16 +331,16 @@ static inline int strfmt(putx_t _put_char, putx_t _put_str, char** dst, const ch
 										padding--;
 										padbuf[padding] = padchar;
 									}
-									_put_str(dst, padbuf);
+									_put_str(fd, dst, padbuf);
 								}
 							}
 
-							_put_str(dst, intbuf);
+							_put_str(fd, dst, intbuf);
 						break;
 
 						case 'x':
 							u = (unsigned int)va_arg(argp, unsigned int);
-							hashflag(_put_str, flags, dst);
+							hashflag(fd, _put_str, flags, dst);
 
 							ditoa((int64_t)u, intbuf, 16);
 
@@ -320,16 +356,16 @@ static inline int strfmt(putx_t _put_char, putx_t _put_str, char** dst, const ch
 										padding--;
 										padbuf[padding] = padchar;
 									}
-									_put_str(dst, padbuf);
+									_put_str(fd, dst, padbuf);
 								}
 							}
 
-							_put_str(dst, intbuf);
+							_put_str(fd, dst, intbuf);
 						break;
 
 						case 'X':
 							u = (unsigned int)va_arg(argp, unsigned int);
-							hashflag(_put_str, flags, dst);
+							hashflag(fd, _put_str, flags, dst);
 
 							ditoa((int64_t)u, intbuf, 16);
 							// TODO this causes liker error in the test cases!! fix that
@@ -354,17 +390,17 @@ static inline int strfmt(putx_t _put_char, putx_t _put_str, char** dst, const ch
 										padding--;
 										padbuf[padding] = padchar;
 									}
-									_put_str(dst, padbuf);
+									_put_str(fd, dst, padbuf);
 								}
 							}
 
-							_put_str(dst, intbuf);
+							_put_str(fd, dst, intbuf);
 
 						break;
 
 						case 'p':
 							v = (void*)va_arg(argp, void*);
-							_put_str(dst, (const char*)"0x");
+							_put_str(fd, dst, (const char*)"0x");
 
 							ditoa((intptr_t)v, intbuf, 16);
 							if((flags&ZERO_FLAG) || (flags&SPACE_FLAG))
@@ -379,35 +415,35 @@ static inline int strfmt(putx_t _put_char, putx_t _put_str, char** dst, const ch
 										padding--;
 										padbuf[padding] = padchar;
 									}
-									_put_str(dst, padbuf);
+									_put_str(fd, dst, padbuf);
 								}
 							}
-							_put_str(dst, intbuf);
+							_put_str(fd, dst, intbuf);
 						break;
 
 						case 'f':
 							d = (double)va_arg(argp, double);
-							plusflag(_put_char, flags, dst);
-							_put_str(dst, ftoa(intbuf, d));
+							plusflag(fd, _put_char, flags, dst);
+							_put_str(fd, dst, ftoa(intbuf, d));
 						break;
 
 						case '%':
 							c = '%';
-							_put_char(dst, &c);
+							_put_char(fd, dst, &c);
 						break;
 					}
 				break;
 
 				// all other charcters
 				default:
-					_put_char(dst, fmt);
+					_put_char(fd, dst, fmt);
 				break;
 			}
 		}
 
 		// null terminate
 		c = '\0';
-		_put_char(dst, &c);
+		_put_char(fd, dst, &c);
 
 		ret = (int)(*dst - start);
 	}
@@ -417,14 +453,14 @@ static inline int strfmt(putx_t _put_char, putx_t _put_str, char** dst, const ch
 
 int vsprintf(char* dst, const char * fmt, va_list argp)
 {
-	return strfmt(memputc, memputs, &dst, fmt, argp);
+	return strfmt(-1, memputc, memputs, &dst, fmt, argp) -1;
 }
 
 int sprintf(char* dst, const char* fmt, ...)
 {
 	va_list argp;
 	va_start(argp, fmt);
-	int ret = strfmt(memputc, memputs, &dst, fmt, argp);
+	int ret = strfmt(-1, memputc, memputs, &dst, fmt, argp) -1;
 	va_end(argp);
 
 	return ret;
@@ -433,7 +469,7 @@ int sprintf(char* dst, const char* fmt, ...)
 int vprintf(const char * fmt, va_list argp)
 {
 	char* dst = NULL;
-	return strfmt(phyputc, phyputs, &dst, fmt, argp);
+	return strfmt(STDOUT_FILENO, phyputc, phyputs, &dst, fmt, argp);
 }
 
 int printf(const char * fmt, ...)
@@ -441,10 +477,97 @@ int printf(const char * fmt, ...)
 	char* dst = NULL;
 	va_list argp;
 	va_start(argp, fmt);
-	int ret = strfmt(phyputc, phyputs, &dst, fmt, argp);
+	int ret = strfmt(STDOUT_FILENO, phyputc, phyputs, &dst, fmt, argp);
 	va_end(argp);
 	return ret;
 }
+
+FILE* fopen(const char * filename, const char * mode)
+{
+	int flags = 0;
+	char m = mode[0];
+	char update = ((mode[1] == '+') || (mode[2] == '+'));
+
+	if(m == 'r')
+		flags = update ? O_RDWR : O_RDONLY;
+	else if(m == 'w')
+		flags = O_CREAT | O_TRUNC | update ? O_RDWR : O_WRONLY;
+	else if(m == 'a')
+		flags = O_CREAT | O_APPEND | update ? O_RDWR : O_WRONLY;
+
+	intptr_t fd = open(filename, flags);
+
+	return fd != EOF ? (FILE*)fd : NULL;
+}
+
+int fclose(FILE* stream)
+{
+	return close((intptr_t)stream);
+}
+
+int fprintf(FILE* stream, const char * fmt, ...)
+{
+	char* dst = NULL;
+	va_list argp;
+	va_start(argp, fmt);
+	int ret = strfmt((intptr_t)stream, phyputc, phyputs, &dst, fmt, argp);
+	va_end(argp);
+	return ret;
+}
+
+int fputc(int character, FILE* stream)
+{
+	return _write((intptr_t)stream, (char*)&character, 1);;
+}
+
+int fputs(const char* str, FILE* stream)
+{
+	return _write((intptr_t)stream, (char*)str, strlen(str));;
+}
+
+int fgetc(FILE* stream)
+{
+	int c = EOF;
+	_read((intptr_t)stream, (char*)&c, 1);
+	return c;
+}
+
+char* fgets(char* str, int num, FILE* stream)
+{
+	char* sptr = str;
+	long int pos = _ftell((intptr_t)stream);
+	int ret = _read((intptr_t)stream, str, num-1);
+
+	// ensure the string is terminated somewhere...
+	if(ret > 0)
+	{
+		sptr[ret] = '\0';
+
+		while((*sptr != '\n') && (*sptr != '\0'))
+			sptr++;
+
+		if(*sptr == '\n')
+		{
+			sptr++;
+			*sptr = '\0';
+		}
+		// set file pointer to the end of the read line
+		_lseek((intptr_t)stream, pos + (sptr - str), SEEK_SET);
+	}
+
+	return (sptr - str) > 0 ? str : NULL;
+}
+
+long int ftell(FILE* stream)
+{
+	return _ftell((intptr_t)stream);
+}
+
+int fseek(FILE * stream, long int offset, int origin)
+{
+	return _lseek((intptr_t)stream, offset, origin);
+}
+
 
 /**
  * @}
--- minstdio.h
+++ minstdio.h
@@ -14,23 +14,6 @@ extern "C" {
 
 #include <stdio.h>
 
-/**
- * physical IO interfac - the implementation must be defined in code
- * that is tailored for the target device or system.
- *
- * Eg for stm32:
-\code
-void phy_putc(char c)
-{
-    while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-    USART_SendData(USART1, (uint16_t)c);
-}
-\endcode
- *
- * @param	c is a character to send via some physical interface.
- */
-void phy_putc(char c);
-
 #ifdef __cplusplus
 }
 #endif
