b3d7c79700a2949046e5ddbf6945b8536e9b18ed
drmetal <spaceorbot@gmail.com>
2014-11-09 18:57:37 +1300

    added licence

--- shell/builtins/builtins.c
+++ shell/builtins/builtins.c
@@ -1,41 +1,68 @@
 /*
- * builtins.c
+ * Copyright (c) 2015 Michael Stuart.
+ * All rights reserved.
  *
- *  Created on: 29/10/2014
- *      Author: stuartm
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ *
+ * This file is part of the nutensils project, <https://github.com/drmetal/nutensils>
+ *
+ * Author: Michael Stuart <spaceorbot@gmail.com>
+ *
  */
 
 #include "builtins.h"
 
-shell_cmd_t help_cmd = {
+/**
+ * @brief   help command - prints all valid commands.
+ */
+int sh_help(int fdes, const char** args, unsigned char nargs)
+{
+    (void)fdes;
+    (void)args;
+    (void)nargs;
+    return SHELL_CMD_PRINT_CMDS;
+}
+
+/**
+ * @brief   exit command - allows the shell to exit.
+ */
+int sh_exit(int fdes, const char** args, unsigned char nargs)
+{
+    (void)fdes;
+    (void)args;
+    (void)nargs;
+    return SHELL_CMD_KILL;
+}
+
+shell_cmd_t sh_help_cmd = {
      .name = "help",
-     .usage = "prints a list of available commands"
+     .usage = "prints a list of available commands",
+     .cmdfunc = sh_help
 };
 
-shell_cmd_t exit_cmd = {
+shell_cmd_t sh_exit_cmd = {
     .name = "exit",
-    .usage = "causes the shell session to terminate"
+    .usage = "causes the shell session to terminate",
+    .cmdfunc = sh_exit
 };
-
-/**
- * @brief	help command - prints all valid commands.
- */
-int help_sh(int fdes, const char** args, unsigned char nargs)
-{
-    (void)fdes;
-    (void)args;
-    (void)nargs;
-	return SHELL_CMD_PRINT_CMDS;
-}
-
-/**
- * @brief	exit command - allows the shell to exit.
- */
-int exit_sh(int fdes, const char** args, unsigned char nargs)
-{
-    (void)fdes;
-    (void)args;
-    (void)nargs;
-	return SHELL_CMD_KILL;
-}
 
