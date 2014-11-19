279594ed2c815111ae0095b3cab9dc65ca393f11
drmetal <spaceorbot@gmail.com>
2014-11-20 09:59:04 +1300

    added in bonny's fix for race condition in get/set time

--- drivers/system_timer/systime.c
+++ drivers/system_timer/systime.c
@@ -36,27 +36,23 @@
 #include "systime.h"
 #include "systime_config.h"
 
-static volatile unsigned long seconds;
+static volatile unsigned long system_seconds;
 
 #define SYSTIMER_TICK_RATE          10000
 #define SYSTIMER_TV_TICK_RATE		1000000
 #define SYSTIMER_PRESCALER			((SYSTIMER_BUS_CLOCK / SYSTIMER_TICK_RATE) - 1)
 #define SYSTIMER_OVERFLOW			(SYSTIMER_TICK_RATE - 1)
 
-SemaphoreHandle_t sys_time_mutex;
 
 void init_systime()
 {
-    seconds = 0;
+    system_seconds = 0;
     if((SYSTIMER_PERIPH == TIM2)||(SYSTIMER_PERIPH == TIM3)||(SYSTIMER_PERIPH == TIM4)||(SYSTIMER_PERIPH == TIM5))
         RCC_APB1PeriphClockCmd(SYSTIMER_CLOCK, ENABLE);
     else if((SYSTIMER_PERIPH == TIM1)||(SYSTIMER_PERIPH == TIM8))
         RCC_APB2PeriphClockCmd(SYSTIMER_CLOCK, ENABLE);
     else
         assert_true(0);
-
-    sys_time_mutex = xSemaphoreCreateMutex();
-    assert_true(sys_time_mutex);
 
     TIM_TimeBaseInitTypeDef timer_init =
     {
@@ -83,21 +79,24 @@ void init_systime()
 void SYSTIMER_INTERRUPT_HANDLER()
 {
     TIM_ClearITPendingBit(SYSTIMER_PERIPH, TIM_IT_Update);
-    seconds++;
+    system_seconds++;
 }
 
 void get_hw_time(unsigned long* secs, unsigned long* usecs)
 {
-    xSemaphoreTake(sys_time_mutex, 0);
+    unsigned long seconds, useconds;
+    __disable_irq();
+    seconds = system_seconds;
+    useconds = SYSTIMER_PERIPH->CNT * (SYSTIMER_TV_TICK_RATE / SYSTIMER_TICK_RATE);
+    __enable_irq();
     *secs = seconds;
-    *usecs = SYSTIMER_PERIPH->CNT * (SYSTIMER_TV_TICK_RATE / SYSTIMER_TICK_RATE);
-    xSemaphoreGive(sys_time_mutex);
+    *usecs = useconds;
 }
 
 void set_hw_time(unsigned long secs, unsigned long usecs)
 {
-    xSemaphoreTake(sys_time_mutex, 0);
-    seconds = secs;
+    __disable_irq();
+    system_seconds = secs;
     SYSTIMER_PERIPH->CNT = usecs / (SYSTIMER_TV_TICK_RATE / SYSTIMER_TICK_RATE);
-    xSemaphoreGive(sys_time_mutex);
+    __enable_irq();
 }
