7d78047ceb7e38eab033e222ba79f2fd236c20ac
drmetal <spaceorbot@gmail.com>
2014-12-11 14:29:11 +1300

    added static version info header file

---
+++ freertos_version.h
@@ -0,0 +1,14 @@
+/*
+ * freertos_version.h
+ *
+ *  Created on: 10/12/2014
+ *      Author: mike stuart
+ */
+
+#ifndef FREERTOS_VERSION_H_
+#define FREERTOS_VERSION_H_
+
+#define FREERTOS_VERSION        "8.0.0:rc2"
+#define FREERTOS_NAME           "FreeRTOS"
+
+#endif /* FREERTOS_VERSION_H_ */
