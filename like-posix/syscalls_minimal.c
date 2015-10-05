9421da9ed918371e6b515627e761d0058c0b5477
drmetal <spaceorbot@gmail.com>
2015-10-05 17:32:17 +1300

    added freertos supported malloc/calloc/free when USE_FREERTOS=1

--- like-posix/syscalls_minimal.c
+++ like-posix/syscalls_minimal.c
@@ -178,3 +178,70 @@ int _wait(int *status)
 	return -1;
 }
 
+#if USE_FREERTOS
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+
+/**
+ *  definition of block structure, copied from heap2 allocator
+ */
+typedef struct A_BLOCK_LINK
+{
+	struct A_BLOCK_LINK *pxNextFreeBlock;	///< The next free block in the list
+	size_t xBlockSize;						///< The size of the free block
+} xBlockLink;
+
+
+static const unsigned short heapSTRUCT_SIZE	=
+ 		( sizeof( xBlockLink ) + portBYTE_ALIGNMENT -
+ 		( sizeof( xBlockLink ) % portBYTE_ALIGNMENT ) );
+
+_PTR _realloc_r(struct _reent *re, _PTR oldAddr, size_t newSize)
+{
+	(void)re;
+
+	xBlockLink *block;
+	size_t toCopy;
+	void *newAddr;
+
+	newAddr = pvPortMalloc(newSize);
+
+	if (newAddr == NULL)
+		return NULL;
+
+	/* We need the block struct pointer to get the current size */
+	block = oldAddr;
+	block -= heapSTRUCT_SIZE;
+
+	/* determine the size to be copied */
+	toCopy = (newSize<block->xBlockSize)?(newSize):(block->xBlockSize);
+
+	/* copy old block into new one */
+	memcpy((void *)newAddr, (void *)oldAddr, (size_t)toCopy);
+
+	vPortFree(oldAddr);
+
+	return newAddr;
+}
+
+_PTR _calloc_r(struct _reent *re, size_t num, size_t size) {
+	(void)re;
+	size *= num;
+    _PTR m = pvPortMalloc(size);
+    if(m)
+        memset(m, 0, size);
+    return m;
+}
+
+_PTR _malloc_r(struct _reent *re, size_t size) {
+	(void)re;
+	return pvPortMalloc(size);
+}
+
+_VOID _free_r(struct _reent *re, _PTR ptr) {
+	(void)re;
+	vPortFree(ptr);
+}
+#endif
