b38c01ce0927e38f69f90168bddde924dafc7f2d
mike <spaceorbot@gmail.com>
2015-10-16 23:36:48 +1300

    first cut of new stdio with file stream table support

--- minlibc/_stdio.h
+++ minlibc/_stdio.h
@@ -31,9 +31,8 @@
  */
 
 /**
- * @addtogroup minstdio
+ * @addtogroup minlibc
  *
- * @file minstdio.h
  * @{
  */
 
@@ -44,11 +43,93 @@
 extern "C" {
 #endif
 
-#include <stdio.h>
 
+/**
+ * buffer size in bytes used by some stdio functions
+ */
+#ifndef MINLIBC_STDIO_BUFFER_SIZE
+#define MINLIBC_STDIO_BUFFER_SIZE   128
+#endif
+
+/**
+ * include floating point string formatting in xxprintf() functions
+ */
 #ifndef MINLIBC_INCLUDE_FLOAT_SUPPORT
 #define MINLIBC_INCLUDE_FLOAT_SUPPORT 1
 #endif
+
+#ifdef MINLIBC_FOPEN_MAX
+#define __FOPEN_MAX__       MINLIBC_FOPEN_MAX
+#else
+#define __FOPEN_MAX__       20
+#endif
+
+#ifdef MINLIBC_BUFSIZ
+#define __BUFSIZ__       MINLIBC_BUFSIZ
+#else
+#define __BUFSIZ__       512
+#endif
+
+#ifdef MINLIBC_FILENAME_MAX
+#define __FILENAME_MAX__    MINLIBC_FILENAME_MAX
+#else
+#define __FILENAME_MAX__       255
+#endif
+
+#ifdef MINLIBC_L_tmpnam
+#define __L_tmpnam__    MINLIBC_L_tmpnam
+#else
+#define __L_tmpnam__    __FILENAME_MAX__
+#endif
+
+#define P_tmpfilename   "/%d.tmp"
+
+//#define TMP_MAX     8
+
+#define FILE_STREAM_TABLE_INDEX_STDIN   0
+#define FILE_STREAM_TABLE_INDEX_STDOUT   1
+#define FILE_STREAM_TABLE_INDEX_STDERR   2
+#define FILE_STREAM_TABLE_START_REGULAR   3
+
+#include <stdio.h>
+
+struct fake__sbuf {
+    char *_base;
+    int _size;
+};
+
+/**
+ * extra small FILE type, replaces newlib FILE
+ */
+typedef struct {
+//    char *_p;  /* current position in (some) buffer */
+//    int   _r;           /* read space left for getc() */
+//    int   _w;           /* write space left for putc() */
+    short _flags;       /* flags, below; this FILE is free if 0 */
+    short _file;        /* fileno, if Unix descriptor, else -1 */
+    struct fake__sbuf _bf;  /* the buffer (at least 1 byte, if !NULL) */
+    struct fake__sbuf _ub;  /* ungetc buffer (at least 1 byte, if !NULL) */
+    char _ubuf[1];   /* guarantee an ungetc() buffer */
+} fake__FILE;
+
+/**
+ * the following must be defined somewhere for stdio to link.
+ * see appleseed syscalls.c
+ */
+
+void init_minlibc();
+
+extern int _open(const char *name, int flags, int mode);
+extern int _close(int file);
+extern int _write(int file, char *buffer, unsigned int count);
+extern int _read(int file, char *buffer, unsigned int count);
+extern int _lseek(int file, int offset, int whence);
+extern long int _ftell(int fd);
+extern int _unlink(char *name);
+extern int _rename(const char *oldname, const char *newname);
+extern int _mkdir(const char *pathname, mode_t mode);
+extern int _fsync(int file);
+
 
 #ifdef __cplusplus
 }
--- minlibc/stdio.c
+++ minlibc/stdio.c
@@ -30,18 +30,58 @@
  */
 
 /**
- * @defgroup minstdio minimal stdio
+ * @defgroup minlibc minimal libc
  *
  * tiny versions of some stdio functions.
  *
- * @file minstdio.c
+
+int vsprintf(char* dst, const char * fmt, va_list argp)
+int sprintf(char* dst, const char* fmt, ...)
+int vprintf(const char * fmt, va_list argp)
+int printf(const char * fmt, ...)
+
+void init_minlibc() (must be called in crt initialization (init_services under appleseed))
+
+int setvbuf(FILE *stream, char *buf, int mode, size_t size) (not functional)
+FILE* fopen(const char * filename, const char * mode)
+FILE* fdopen(int fd, const char *mode)
+FILE* freopen(const char* filename, const char* mode, FILE* file)
+int fclose(FILE* stream)
+int fprintf(FILE* stream, const char * fmt, ...)
+int getc(FILE* stream) (not provided)
+int fgetc(FILE* stream)
+int ungetc(int c, FILE* stream)
+int fputc(int character, FILE* stream)
+int putc(int character, FILE* stream) (not provided)
+int fputs(const char* str, FILE* stream)
+char* fgets(char* str, int num, FILE* stream)
+long int ftell(FILE* stream)
+int fseek(FILE * stream, long int offset, int origin)
+size_t fwrite(const void *data, size_t size, size_t count, FILE *stream)
+size_t fread(void *data, size_t size, size_t count, FILE *stream)
+int fflush(FILE* stream)
+int fileno(FILE* stream) (possibly broken)
+int rename(const char *oldname, const char *newname)
+int remove(const char *name)
+FILE* tmpfile(void)
+char* tmpnam(char *result)
+
+Note: the maximum number of open files depends on a few different settings:
+_FS_LOCK: specified in ffconf.h, set in fatfs.mk, and can be set in the project makefile.
+            sets the max number of simultaneously open regular files.
+MINLIBC_FOPEN_MAX: specified in _stdio.h, my be overidden from the project makefile.
+            sets the number of files in the file stream table (can be any type of file).
+FILE_TABLE_LENGTH: specified in like_posix_config.h (per project).
+            sets the number of files in the file descriptor table (can be any type of file)
+
  * @{
  */
 
+#include <sys/reent.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <math.h>
-#include <unistd.h> // open(), close() etc
+#include <unistd.h> // STDOUT_FILENO etc
 #include <ctype.h> // isdigit() etc
 #include <fcntl.h>
 
@@ -50,6 +90,10 @@
 #include "_stdio.h"
 #include "_stdlib.h"
 #include "_string.h"
+
+fake__FILE __fstab[FOPEN_MAX];
+static char __tmpnambuf[L_tmpnam];
+fake__FILE* __tmpfs[TMP_MAX];
 
 #define PLUS_FLAG 1
 #define MINUS_FLAG 2
@@ -62,12 +106,7 @@
 
 typedef char*(*putx_t)(int, char**, const char*);
 
-extern int _write(int file, char *buffer, unsigned int count);
-extern int _read(int file, char *buffer, unsigned int count);
-extern int _lseek(int file, int offset, int whence);
-extern long int _ftell(int fd);
-
-static inline char* phyputc(int fd, char** dst, const char*src)
+static inline char* __minlibc_putc(int fd, char** dst, const char*src)
 {
 	if(*src)
 	{
@@ -77,7 +116,7 @@ static inline char* phyputc(int fd, char** dst, const char*src)
 	return *dst;
 }
 
-static inline char* phyputs(int fd, char** dst, const char*src)
+static inline char* __minlibc_puts(int fd, char** dst, const char*src)
 {
 	int l = strlen(src);
 	if(l > 0)
@@ -245,6 +284,21 @@ static inline int strfmt(int fd, putx_t _put_char, putx_t _put_str, char** dst, 
 							s = (char*)va_arg(argp, char*);
 							if(!s)
 								s = (const char*)"(null)";
+							if(flags&SPACE_FLAG)
+							{
+								padding -= strlen(s);
+								if(padding > 0)
+								{
+									char padbuf[padding+1];
+									padbuf[padding] = '\0';
+									while(padding)
+									{
+										padding--;
+										padbuf[padding] = padchar;
+									}
+									_put_str(fd, dst, padbuf);
+								}
+							}
 							_put_str(fd, dst, s);
 						break;
 
@@ -433,7 +487,7 @@ int sprintf(char* dst, const char* fmt, ...)
 int vprintf(const char * fmt, va_list argp)
 {
 	char* dst = NULL;
-	return strfmt(STDOUT_FILENO, phyputc, phyputs, &dst, fmt, argp);
+	return strfmt(STDOUT_FILENO, __minlibc_putc, __minlibc_puts, &dst, fmt, argp);
 }
 
 int printf(const char * fmt, ...)
@@ -441,120 +495,407 @@ int printf(const char * fmt, ...)
 	char* dst = NULL;
 	va_list argp;
 	va_start(argp, fmt);
-	int ret = strfmt(STDOUT_FILENO, phyputc, phyputs, &dst, fmt, argp);
+	int ret = strfmt(STDOUT_FILENO, __minlibc_putc, __minlibc_puts, &dst, fmt, argp);
 	va_end(argp);
 	return ret;
 }
 
+
+/**
+ * this function initializes the FILE stream table, no f-functions should be used before this has been called.
+ */
+void init_minlibc()
+{
+    _impure_ptr->_stdin = (__FILE*)&__fstab[FILE_STREAM_TABLE_INDEX_STDIN];
+    _impure_ptr->_stdout = (__FILE*)&__fstab[FILE_STREAM_TABLE_INDEX_STDOUT];
+    _impure_ptr->_stderr = (__FILE*)&__fstab[FILE_STREAM_TABLE_INDEX_STDERR];
+
+    short i;
+
+    for(i=0; i < FOPEN_MAX; i++)
+    {
+        __fstab[i]._ub._base = __fstab[i]._ubuf;
+        __fstab[i]._ub._size = 0;
+        __fstab[i]._bf._base = NULL;
+        __fstab[i]._bf._size = 0;
+        __fstab[i]._flags = 0;
+        __fstab[i]._file = -1;
+    }
+
+    __fstab[FILE_STREAM_TABLE_INDEX_STDIN]._file = STDIN_FILENO;
+    __fstab[FILE_STREAM_TABLE_INDEX_STDIN]._flags = __SRD|__SNBF;
+
+    __fstab[FILE_STREAM_TABLE_INDEX_STDOUT]._file = STDOUT_FILENO;
+    __fstab[FILE_STREAM_TABLE_INDEX_STDOUT]._flags = __SWR|__SNBF;
+
+    __fstab[FILE_STREAM_TABLE_INDEX_STDERR]._file = STDERR_FILENO;
+    __fstab[FILE_STREAM_TABLE_INDEX_STDERR]._flags = __SWR|__SNBF;
+}
+
+static inline FILE* __get_stream_descriptor(int fdes, int flags)
+{
+    short i;
+    if(fdes == EOF)
+        return NULL;
+    else if(fdes == STDIN_FILENO)
+        return (FILE*)&__fstab[FILE_STREAM_TABLE_INDEX_STDIN];
+    else if(fdes == STDOUT_FILENO)
+        return (FILE*)&__fstab[FILE_STREAM_TABLE_INDEX_STDOUT];
+    else if(fdes == STDERR_FILENO)
+        return (FILE*)&__fstab[FILE_STREAM_TABLE_INDEX_STDERR];
+
+    // add 1 to make checking easier with FREAD/FWRITE
+    flags++;
+
+    for(i=FILE_STREAM_TABLE_START_REGULAR; i<FOPEN_MAX; i++)
+    {
+        if(__fstab[i]._flags == 0)
+        {
+            if(flags & FREAD)
+                __fstab[i]._flags |= __SRD;
+            else if(flags & FWRITE)
+                __fstab[i]._flags |= __SWR;
+            else if(flags & (FREAD|FWRITE))
+                __fstab[i]._flags |= __SRW;
+
+            __fstab[i]._file = fdes;
+
+            return (FILE*)&__fstab[i];
+        }
+    }
+    return NULL;
+}
+
+void __release_stream_descriptor(FILE* stream)
+{
+    fake__FILE* s = (fake__FILE*)stream;
+    if(stream && stream != stdin && stream != stdout && stream != stderr)
+    {
+        s->_ub._size = 0;
+        s->_bf._size = 0;
+        s->_flags = 0;
+        s->_file = -1;
+    }
+}
+
+static inline int __get_fileno(FILE* stream)
+{
+    fake__FILE* s = (fake__FILE*)stream;
+    return s ? s->_file : EOF;
+}
+
+static inline int __stream_mode(const char* mode)
+{
+    int flags = 0;
+    char m = mode[0];
+    char update = ((mode[1] == '+') || (mode[2] == '+'));
+
+    if(m == 'r')
+        flags = update ? O_RDWR : O_RDONLY;
+    else if(m == 'w')
+        flags = O_CREAT | O_TRUNC | (update ? O_RDWR : O_WRONLY);
+    else if(m == 'a')
+        flags = O_CREAT | O_APPEND | (update ? O_RDWR : O_WRONLY);
+
+    return flags;
+}
+
+static inline int __find_tmpindex()
+{
+    int i;
+    for(i = 0; i < TMP_MAX; i++)
+    {
+        if(!__tmpfs[i])
+            break;
+    }
+    return i == TMP_MAX ? -1 : i;
+}
+
+/**
+ * not really implemented, will work OK if the buffer is se to NULL, ie specifying system allocated buffer,
+ * and when mode is set to _IOFBF. other modes will simply behave as for _IOFBF.
+ */
+int setvbuf(FILE *stream, char *buf, int mode, size_t size)
+{
+    fake__FILE* s = (fake__FILE*)stream;
+    if(buf)
+        return -1;
+
+    switch(mode)
+    {
+        case _IOFBF:       /* setvbuf should set fully buffered */
+            s->_bf._base = buf;
+            s->_bf._size = size;
+        break;
+        case _IOLBF:       /* setvbuf should set line buffered */
+            s->_bf._base = buf;
+            s->_bf._size = size;
+        break;
+        case _IONBF:       /* setvbuf should set unbuffered */
+            s->_bf._base = buf;
+            s->_bf._size = size;
+        break;
+    }
+
+    return 0;
+}
+
+/**
+ * Note:
+ *      the same file may be opened more than once (and used, this is according to the policy of the like-posix _open() system call)
+ *      the file must be closed once for every open made.
+ */
 FILE* fopen(const char * filename, const char * mode)
 {
-	int flags = 0;
-	char m = mode[0];
-	char update = ((mode[1] == '+') || (mode[2] == '+'));
+    FILE* file = NULL;
+    int flags = __stream_mode(mode);
 
-	if(m == 'r')
-		flags = update ? O_RDWR : O_RDONLY;
-	else if(m == 'w')
-		flags = O_CREAT | O_TRUNC | (update ? O_RDWR : O_WRONLY);
-	else if(m == 'a')
-		flags = O_CREAT | O_APPEND | (update ? O_RDWR : O_WRONLY);
+    int fd = _open(filename, flags, MINLIBC_STDIO_BUFFER_SIZE);
 
-	intptr_t fd = open(filename, flags);
+    if(fd != EOF)
+    {
+        file = __get_stream_descriptor(fd, flags);
+        if(!file)
+            _close(fd);
+    }
 
-	return fd != EOF ? (FILE*)fd : NULL;
+    return file;
 }
 
 FILE* fdopen(int fd, const char *mode)
 {
-    (void)mode;
-    if(fd != -1)
-        return (FILE*)fd;
-    return NULL;
+    return __get_stream_descriptor(fd, __stream_mode(mode));
+}
+
+FILE* freopen(const char* filename, const char* mode, FILE* stream)
+{
+    _fsync(__get_fileno(stream));
+    fclose(stream);
+    return fopen(filename, mode);
 }
 
 int fclose(FILE* stream)
 {
-	return close((intptr_t)stream);
+    char buf[16];
+    int i = 0;
+    int res = EOF;
+
+    if(stream)
+    {
+        if(stream != stdin && stream != stdout && stream != stderr)
+            res = _close(__get_fileno(stream));
+
+        __release_stream_descriptor(stream);
+
+        while(i < TMP_MAX)
+        {
+            if(__tmpfs[i] == (fake__FILE*)stream)
+            {
+                __tmpfs[i] = NULL;
+                sprintf(buf, P_tmpdir P_tmpfilename, i);
+                _unlink(buf);
+                break;
+            }
+            i++;
+        }
+    }
+
+    return res;
 }
 
 int fprintf(FILE* stream, const char * fmt, ...)
 {
-	char* dst = NULL;
-	va_list argp;
-	va_start(argp, fmt);
-	int ret = strfmt((intptr_t)stream, phyputc, phyputs, &dst, fmt, argp);
-	va_end(argp);
-	return ret;
+    char* dst = NULL;
+    va_list argp;
+    va_start(argp, fmt);
+    int ret = strfmt(__get_fileno(stream), __minlibc_putc, __minlibc_puts, &dst, fmt, argp);
+    va_end(argp);
+    return ret;
+}
+
+#ifndef getc
+int getc(FILE* stream)
+{
+    return fgetc(stream);
+}
+#else
+#pragma message("getc is defined outside minlibc - it is probably not safe to use it")
+#endif
+
+int fgetc(FILE* stream)
+{
+    fake__FILE* s = (fake__FILE*)stream;
+    int c = EOF;
+    if(s->_ub._size > 0)
+    {
+        s->_ub._size--;
+        c = s->_ub._base[s->_ub._size];
+    }
+    else
+        _read(__get_fileno(stream), (char*)&c, 1);
+    return c;
+}
+
+int ungetc(int c, FILE* stream)
+{
+    fake__FILE* s = (fake__FILE*)stream;
+
+    if(c == EOF)
+        return EOF;
+    if(s->_ub._size < (int)sizeof(s->_ubuf))
+    {
+        s->_ub._base[s->_ub._size] = c;
+        s->_ub._size++;
+        return 0;
+    }
+    return EOF;
 }
 
 int fputc(int character, FILE* stream)
 {
-	return _write((intptr_t)stream, (char*)&character, 1);;
+    return _write(__get_fileno(stream), (char*)&character, 1);;
 }
+
+#ifndef putc
+int putc(int character, FILE* stream)
+{
+    return _write(__get_fileno(stream), (char*)&character, 1);;
+}
+#else
+#pragma message("putc is defined outside minlibc - it is probably not safe to use it")
+#endif
 
 int fputs(const char* str, FILE* stream)
 {
-	return _write((intptr_t)stream, (char*)str, strlen(str));;
+    return _write(__get_fileno(stream), (char*)str, strlen(str));;
 }
 
-int fgetc(FILE* stream)
-{
-	int c = EOF;
-	_read((intptr_t)stream, (char*)&c, 1);
-	return c;
-}
+//char* fgets(char* str, int num, FILE* stream)
+//{
+//    char* sptr = str;
+//    int i;
+//    int len;
+//    for(i = 0; i < num-1; )
+//    {
+//        len = _read(__get_fileno(stream), str, 1);
+//
+//        if(len == -1)
+//            break;
+//        else if(len)
+//        {
+//            if(*str == '\n')
+//            {
+//                i++;
+//                str++;
+//                break;
+//            }
+//            str++;
+//            i++;
+//        }
+//    }
+//    *str = 0;
+//
+//    return sptr == str ? NULL : sptr;
+//}
+
 
 char* fgets(char* str, int num, FILE* stream)
 {
-	char* sptr = str;
-	long int pos = _ftell((intptr_t)stream);
-	int ret = _read((intptr_t)stream, str, num-1);
+    char* sptr = str;
+    int fd = __get_fileno(stream);
+    long int pos = _ftell(fd);
+    int ret = _read(fd, str, num-1);
 
-	// ensure the string is terminated somewhere...
-	if(ret > 0)
-	{
-		sptr[ret] = '\0';
+    // ensure the string is terminated somewhere...
+    if(ret > 0)
+    {
+        sptr[ret] = '\0';
 
-		while((*sptr != '\n') && (*sptr != '\0'))
-			sptr++;
+        while((*sptr != '\n') && (*sptr != '\0'))
+            sptr++;
 
-		if(*sptr == '\n')
-		{
-			sptr++;
-			*sptr = '\0';
-		}
-		// set file pointer to the end of the read line
-		_lseek((intptr_t)stream, pos + (sptr - str), SEEK_SET);
-	}
+        if(*sptr == '\n')
+        {
+            sptr++;
+            *sptr = '\0';
+        }
+        // set file pointer to the end of the read line
+        _lseek(fd, pos + (sptr - str), SEEK_SET);
+    }
 
-	return (sptr - str) > 0 ? str : NULL;
+    return (sptr - str) > 0 ? str : NULL;
 }
 
 long int ftell(FILE* stream)
 {
-	return _ftell((intptr_t)stream);
+    return _ftell(__get_fileno(stream));
 }
 
 int fseek(FILE * stream, long int offset, int origin)
 {
-	return _lseek((intptr_t)stream, offset, origin);
+    fake__FILE* s = (fake__FILE*)stream;
+    s->_ub._size = 0;
+    return _lseek(__get_fileno(stream), offset, origin);
 }
 
 size_t fwrite(const void *data, size_t size, size_t count, FILE *stream)
 {
-	return _write((intptr_t)stream, (char*)data, size*count);
+    return _write(__get_fileno(stream), (char*)data, size*count);
 }
 
 size_t fread(void *data, size_t size, size_t count, FILE *stream)
 {
-	return _read((intptr_t)stream, (char*)data, size*count);
+    return _read(__get_fileno(stream), (char*)data, size*count);
 }
 
 int fflush(FILE* stream)
 {
-    return fsync((intptr_t)stream);
+    return _fsync(__get_fileno(stream));
 }
 
+int fileno(FILE* stream)
+{
+    return __get_fileno(stream);
+}
+
+int rename(const char *oldname, const char *newname)
+{
+    return _rename(oldname, newname);
+}
+
+int remove(const char *name)
+{
+    return _unlink((char*)name);
+}
+
+FILE* tmpfile(void)
+{
+    int i = __find_tmpindex();
+
+    if(i != -1)
+    {
+        _mkdir(P_tmpdir, 0777);
+        sprintf(__tmpnambuf, P_tmpdir P_tmpfilename, i);
+        __tmpfs[i] = (fake__FILE*)fopen(__tmpnambuf, "w+");
+        return (FILE*)__tmpfs[i];
+    }
+
+    return  NULL;
+}
+
+char* tmpnam(char *result)
+{
+    char* buf = result ? result : __tmpnambuf;
+    int i = __find_tmpindex();
+
+    if(i != -1)
+    {
+        sprintf(buf, P_tmpdir P_tmpfilename, i);
+        return buf;
+    }
+
+    return NULL;
+}
 
 
 
