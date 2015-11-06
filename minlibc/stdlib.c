cc854c28d0982d8b8e4a1002313ce38c937684e4
mike <spaceorbot@gmail.com>
2015-11-06 23:01:06 +1300

    added rand, srand, system, strtof, atof functions

--- minlibc/stdlib.c
+++ minlibc/stdlib.c
@@ -39,14 +39,83 @@
  * @{
  */
 
-//#include <stdint.h>
-//#include <stddef.h>
-//#include <math.h>
-//
-//#include "_stdlib.h"
-//#include "_string.h"
+#include <stdint.h>
+#include <stddef.h>
+#include <math.h>
+#include <minlibc/stdlib.h>
+#include <string.h>
 
+double atof(const char* string)
+{
+    const char* mantissa = strchr(string, '.');
+    const char* characteristic = string;
 
+    float result = atoi(characteristic);
+    if(mantissa)
+    {
+        mantissa++;
+        result += atoi(mantissa) / (strlen(mantissa) * 10);
+    }
+    return (double)result;
+}
+
+float strtof(const char *string, char **tailptr)
+{
+    float result = NAN;
+    while(*string == ' ' || *string == '\t')
+        string++;
+
+    if(*string)
+    {
+        result = atof(string);
+        if(tailptr && *tailptr)
+        {
+            while(*string == ' ' || *string == '\t')
+                string++;
+            *tailptr = (char*)string;
+        }
+    }
+
+    return result;
+}
+
+char* getenv(const char* name)
+{
+    (void)name;
+    return NULL;
+}
+
+static unsigned int __rand_xn = 1;
+#define  __rand_a  1103515245
+#define  __rand_c  12345
+#define  __rand_m  2147483647
+
+void srand(unsigned int seed)
+{
+    __rand_xn = seed;
+}
+
+int rand()
+{
+    __rand_xn = (__rand_a * __rand_xn + __rand_c) % __rand_m;
+    return __rand_xn;
+}
+
+#if USE_SHELL
+#pragma message("compiling stdlib system() with shell support")
+#else
+#pragma message("compiling stdlib system() without shell support")
+#endif
+
+int system(const char *command)
+{
+#if USE_SHELL
+    return _system(command);
+#else
+    (void)command;
+    return -1;
+#endif
+}
 
 /**
  * @}
