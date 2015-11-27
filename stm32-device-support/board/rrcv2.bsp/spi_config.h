8e9c1ed1c0eb2c3a65c1c7ce3c124fefca343b9a
drmetal <spaceorbot@gmail.com>
2015-11-27 15:58:55 +1300

    added blank SPI configs

---
+++ stm32-device-support/board/rrcv2.bsp/spi_config.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2015 Michael Stuart.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ *
+ * This file is part of the Appleseed project, <https://github.com/drmetal/appleseed>
+ *
+ * Author: Michael Stuart <spaceorbot@gmail.com>
+ *
+ */
+
+#ifndef BOARD_SPI_H_
+#define BOARD_SPI_H_
+
+#define SPI1_FULL_REMAP 0
+#define SPI3_FULL_REMAP 0
+
+/**
+ * only specify chip select's for those SPI's that require them.
+ * (comment out those that are not needed)
+ */
+//#define SPI1_NSS_PORT 	GPIO
+//#define SPI1_NSS_PIN 	GPIO_PIN_
+//#define SPI2_NSS_PORT 	GPIO
+//#define SPI2_NSS_PIN 	GPIO_PIN_
+//#define SPI2_NSS_PORT 	GPIO
+//#define SPI2_NSS_PIN 	GPIO_PIN_
+
+#endif /* BOARD_SPI_H_ */
+
