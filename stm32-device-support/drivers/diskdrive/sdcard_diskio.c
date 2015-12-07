073c1992037b74a67071ae6e23cd941f0399b661
drmetal <spaceorbot@gmail.com>
2015-12-08 09:50:23 +1300

    merged

---
+++ stm32-device-support/drivers/diskdrive/sdcard_diskio.c
@@ -0,0 +1,232 @@
+/*
+ * Copyright (c) 2015 Michael Stuart.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ *
+ * This file is part of the Appleseed project, <https://github.com/drmetal/app-l-seed>
+ *
+ * Author: Michael Stuart <spaceorbot@gmail.com>
+ *
+ */
+
+#include "sdcard_diskio.h"
+
+#include <time.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "sdcard.h"
+
+
+static void sdcard_initialize(disk_interface_t* disk);
+static DRESULT sdcard_read(disk_interface_t* disk, BYTE *buff, DWORD sector, UINT count);
+static DRESULT sdcard_write(disk_interface_t* disk, const BYTE *buff, DWORD sector, UINT count);
+static DRESULT sdcard_ioctl(disk_interface_t* disk, BYTE ctrl, void *buff);
+static void sdcard_status(disk_interface_t* disk);
+
+
+FRESULT sdcard_mount(disk_interface_t* disk, int drive)
+{
+	FRESULT res;
+	disk->disk_initialize = sdcard_initialize;
+	disk->disk_read = sdcard_read;
+	disk->disk_write = sdcard_write;
+	disk->disk_ioctl = sdcard_ioctl;
+	disk->disk_status = sdcard_status;
+
+	sprintf(disk->mapping.drivemapping, "%d:", drive);
+	disk->mapping.drivename[0] = '\0';
+	disk->mapping.mountpoint = "/";
+
+	res = f_mount(&disk->mapping.fs, disk->mapping.drivemapping, 1);
+	return res;
+}
+
+void sdcard_initialize(disk_interface_t* disk)
+{
+	SD_CardInfo cardinfo;
+
+    disk->status = STA_NOINIT | STA_PROTECT | STA_NODISK;
+
+    if((SD_Detect() == SD_PRESENT))
+        disk->status &= ~STA_NODISK;
+
+    if(SD_WPDetect() != SD_WRITE_PROTECTED)
+        disk->status &= ~STA_PROTECT;
+
+    if(disk->status == STA_NOINIT)
+    {
+        if(SD_Init(&cardinfo) == SD_OK)
+        {
+            disk->status &= ~STA_NOINIT;
+
+            disk->info.block_size = cardinfo.CardBlockSize;
+            disk->info.erase_block_size = cardinfo.SD_csd.EraseGrSize;
+            disk->info.type = cardinfo.CardType;
+            disk->info.capacity = cardinfo.CardCapacity;
+        }
+    }
+}
+
+DRESULT sdcard_read(disk_interface_t* disk, BYTE *buff, DWORD sector, UINT count)
+{
+    DRESULT res = RES_ERROR;
+    SD_Error err = SD_OK;
+    SDCardState cardstate = SD_CARD_ERROR;
+
+    if(disk->status & (STA_NODISK | STA_NOINIT))
+        return RES_NOTRDY;
+
+
+    if(count == 1)
+        err = SD_ReadBlock((uint8_t*)buff, sector);
+    else
+        err = SD_ReadMultiBlocks((uint8_t*)buff, sector, count);
+
+    if(err == SD_OK)
+        err = SD_WaitIOOperation(WAIT_WHILE_RX_ACTIVE);
+
+    while(err == SD_OK)
+    {
+        err = SD_QueryStatus(&cardstate);
+        if(cardstate == SD_CARD_TRANSFER)
+        {
+            res = RES_OK;
+            break;
+        }
+        usleep(1000);
+    }
+
+    return res;
+}
+
+DRESULT sdcard_write (disk_interface_t* disk, const BYTE *buff, DWORD sector, UINT count)
+{
+    DRESULT res = RES_ERROR;
+    SD_Error err = SD_OK;
+    SDCardState cardstate = SD_CARD_ERROR;
+
+    if(disk->status & (STA_NODISK | STA_NOINIT))
+        return RES_NOTRDY;
+
+    if(disk->status & STA_PROTECT)
+        return RES_WRPRT;
+
+    if(count == 1)
+        err = SD_WriteBlock((const uint8_t*)buff, sector);
+    else if(count > 1)
+        err = SD_WriteMultiBlocks((const uint8_t*)buff, sector, count);
+
+    if(err == SD_OK)
+        err = SD_WaitIOOperation(WAIT_WHILE_TX_ACTIVE);
+
+    while(err == SD_OK)
+    {
+        err = SD_QueryStatus(&cardstate);
+        if(cardstate == SD_CARD_TRANSFER)
+        {
+            res = RES_OK;
+            break;
+        }
+        usleep(1000);
+    }
+
+    return res;
+}
+
+
+DRESULT sdcard_ioctl(disk_interface_t* disk, BYTE ctrl, void *buff)
+{
+   DRESULT res = RES_OK;
+
+    if(disk->status & (STA_NODISK | STA_NOINIT))
+        return RES_NOTRDY;
+
+    switch (ctrl)
+    {
+      case CTRL_SYNC :        // error if transfer not complete
+          if(SD_GetTransferState() == SD_TRANSFER_BUSY)
+              res = RES_ERROR;
+      break;
+      case GET_SECTOR_COUNT : // Get number of sectors on the disk
+          *(DWORD*)buff = disk->info.capacity;
+      break;
+      case GET_SECTOR_SIZE :  // Get R/W sector size
+          *(DWORD*)buff = disk->info.block_size;
+      break;
+      case GET_BLOCK_SIZE :   // Get erase block size in unit of sector
+          *(DWORD*)buff = disk->info.erase_block_size;
+      break;
+      case MMC_GET_TYPE :     // Get card type
+          *(BYTE*)buff = disk->info.type;
+      break;
+      case MMC_GET_CSD:
+
+      break;
+      case MMC_GET_CID:
+
+      break;
+      case MMC_GET_OCR:
+
+      break;
+      case MMC_GET_SDSTAT:
+
+      break;
+      case CTRL_POWER :
+        if(*(BYTE*)buff == 0)
+            SD_PowerOFF();
+        if(*(BYTE*)buff == 1)
+            SD_PowerON();
+      break;
+//      case CTRL_ERASE_SECTOR:
+//          SD_Erase(*(((DWORD*)buff)), *(((DWORD*)buff)+1));
+//      break;
+      default:
+         res = RES_PARERR;
+      break;
+    }
+
+    return res;
+}
+
+/**
+ * if disk is present, clears STA_NODISK flag.
+ * else sets STA_NODISK flag.
+ * if disk is not write protected, clears SD_WRITE_PROTECTED flag.
+ * else sets SD_WRITE_PROTECTED flag.
+ */
+void sdcard_status(disk_interface_t* disk)
+{
+    // update status, based on card inserted state
+
+    // SD card must be present and drive number set to 0
+    if((SD_Detect() == SD_PRESENT))
+       disk->status &= ~STA_NODISK;         // indicate disk present
+   else
+       disk->status |= STA_NODISK;         // indicate no-disk
+
+    if(SD_WPDetect() == SD_WRITE_PROTECTED)
+       disk->status |= STA_PROTECT;     // indicate write protected
+   else
+       disk->status &= ~STA_PROTECT;     // indicate not write protected
+}
